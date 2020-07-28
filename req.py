d5dc318f50fc3d5a606bb58bd2db6a12ce6a2a77
Nikolay Krasko <nikolaykrasko@MacBook-Air-Nikolay.local>
2020-07-29 02:05:52 +0300

    Version 1.1

--- req.py
+++ req.py
@@ -18,16 +18,19 @@ def req(url):
     response = requests.get(url)
     return response.status_code
 
+def send_message(text):
+    bot.send_message(CHAT_ID, text)
+
 def check_list(urls):
     '''
     Принимает список адресов, если у какого-либо
     адреса статус не равен 200 - отправляет
     сообщение в заданный чат тг
     '''
-    for url in urls.values():
+    for url in urls:
         status = req(url)
         if status != 200:
-            bot.send_message(CHAT_ID, 'Status {} for url: {}'.format(status, url))
+            send_message('Status {} for url: {}'.format(status, url))
 
 def add_url(message):
     '''
@@ -38,28 +41,76 @@ def add_url(message):
         js = json.load(f)
     if message.text not in js.values():
         with open('urls.json', 'w') as f:
-            js[len(js)] = message.text
+            js[message.text] = len(js.values())
             json.dump(js, f)
 
+def delete_url(message):
+    url = message.text
+    with open('urls.json', 'w') as f:
+        js = json.load(f)
+        js.pop(url)
+        json.dump(js, f)
+
+def status_check(atribut = 'message'):
+    '''
+    На вход подается атрибут, который может принимать значения message, 
+    callback и date. проводит проверку раз в 5 секунд в течении 30 секунд
+    на наличие в updates объекта, соответствующего атрибуту. В случае 
+    появления объекта - возвращает его, если не обнаружит - вернет False
+    '''
+    now = time.time()
+    while time.time() - now < 30:
+        time.sleep(2)
+        try:
+            if atribut == 'message':
+                data = bot.get_updates()[-1].message
+                return data
+            elif atribut == 'callback':
+                data = bot.get_updates()[-1].callback_query
+                return data
+        except AttributeError:
+            pass
+    return False   
+
 def callback_check():
-    callback = bot.get_updates[-1].callback_query
+    '''
+    Обработчик кнопок
+    '''
+    callback = status_check('callback')
+    if not callback:
+        return
     if callback.data == 'new':
-        send_message()
+        send_message('Введите адрес сайта')
+        message = status_check()
+        if message: add_url(message) 
+                 
+    elif callback.data == 'alls':
+        with open('urls.json') as f:
+            js = json.load(f)
+        for i in js.keys():
+            send_message(i)
+    elif callback.data == 'dels':
+        send_message('Введите адрес сайта, который хотите удалить')
+        mesage = status_check()
+        if mesage: delete_url(mesage)
 
 def main():
     update_time = time.time()
     check_time = time.time()
     while True:
-        if time.time() - check_time >= 30:
+        if time.time() - check_time >= 300:
             with open('urls.json') as f:
                 urls = json.load(f)
             check_list(urls)
             check_time = time.time()
 
-        if time.time() - update_time >= 20:
-            message = bot.get_updates()[-1].message
-            '''
-            if time.time() - message.date < 30:
+        if time.time() - update_time >= 10:
+            
+            try:
+                date = bot.get_updates()[-1].message.date
+            except AttributeError:
+                date = False
+            if time.time() - date < 20:
                 keyboard = types.InlineKeyboardMarkup()
                 new_data = types.InlineKeyboardButton(text='Добавить новый сайт', callback_data='new')
                 keyboard.add(new_data)
@@ -68,9 +119,9 @@ def main():
                 dal_data = types.InlineKeyboardButton(text='Удалить сайт', callback_data='dels')
                 keyboard.add(dal_data)
                 bot.send_message(CHAT_ID, 'Здравствуйте.', reply_markup=keyboard)
-            '''
-            if message.text.split('//')[0] in ['http:','https:']: 
-                add_url(message)
+                time.sleep(3)
+                callback_check()
+            
             update_time = time.time()
         
 
--- tst.py
+++ tst.py
@@ -14,17 +14,7 @@ for url in urls.values():
     print(url)
     print(requests.get(url).status_code)
 
+
 print(bot.get_updates()[-1].message.chat.id)
-print(bot.get_updates()[-1].message.text)
 '''
-url ='http://www.example.org/index.asp'
-#print(code(url))
-def timer(start):
-    while
-
-
-@bot.message_handler()
-def txt(message):
-    print(message.text)
-    timer(message.date)
-bot.polling()
+print(bot.get_updates(limit=1)[0].message.date)
